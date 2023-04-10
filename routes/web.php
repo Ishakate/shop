f77e6357c487cd30c7560a246c8daae01c4c1a6d
Marcin Prokopek <marcpro772@student.polsl.pl>
2023-04-10 21:37:42 +0200

    list of users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Usercontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/lsit', [Usercontroller::class,'index'])->middleware('auth');
+    
 
 
 Auth::routes();
